d4cfe3d563a530736ed1136391b33e8e4dae2e97
Christopher Sumnicht <csumnicht@berkeley.edu>
2025-10-15 19:39:02 -0700

    Redid tests a bit, some pass now.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+from io import StringIO
+from contextlib import redirect_stdout
 import os
 import pytest
 import subprocess
@@ -10,23 +12,27 @@ TEST_INPUTS = "tests/test_inputs/"
 TEST_OUTPUTS = "tests/test_outputs/"
 # Each input file has a corresponding output file.
 INPUT_FILES = sorted(os.listdir(TEST_INPUTS))
-OUTPUT_FILES = sorted(os.listdir(TEST_OUTPUTS))
 
 
 def run(ruru_code):
     rust_code = transpile(lexer(ruru_code))
     with open('temp.rs', mode="w") as rust_file:
         rust_file.write(rust_code)
-    subprocess.run(["rustc", 'temp.rs', "-o", "temp_binary"])
-    output = subprocess.run(["./temp_binary"], capture_output=True)
+    subprocess.run(["rustc", 'temp.rs', 
+                    "-o", "temp_binary"])
+    output = subprocess.run(["./temp_binary"], capture_output=True).stdout
     pathlib.Path("temp_binary").unlink()
-    pathlib.Path("../temp.rs").unlink()
+    pathlib.Path("temp.rs").unlink()
     return output
 
 
-@pytest.mark.parametrize("in_, out", zip(INPUT_FILES, OUTPUT_FILES))
-def test_outputs(in_, out):
-    assert run(in_) == run(out)
+@pytest.mark.parametrize("file_name", INPUT_FILES)
+def test_outputs(file_name):
+    with open(TEST_INPUTS + file_name) as file:
+        exec_output = StringIO()
+        with redirect_stdout(exec_output):
+            exec(file.read())
+        assert run(file.read()) == exec_output.getvalue().encode("utf-8")
 
 
 def test_rust_outputs():
